283451f6957ca48beeedf107530b244d8d09d0c7
ichiaki907 <ichiaki907@gmail.com>
2023-12-31 02:09:47 +0900

    Refactor image to text script and add continuous processing

--- image2text/image_to_text_script.py
+++ image2text/image_to_text_script.py
@@ -1,58 +1,63 @@
 import base64
+import time
 import cv2
 import openai
 import os
-import time
 from datetime import datetime, timedelta
 import numpy as np
 from dotenv import load_dotenv
 
-# .envファイルの内容を読み込見込む
+def encode_image_to_base64(image_path):
+    frame = cv2.imread(image_path)  # 画像を読み込む
+    _, buffer = cv2.imencode(".jpg", frame)
+    return base64.b64encode(buffer).decode('utf-8')
+
+# .envファイルの内容を読み込む
 load_dotenv()
 
 # 画像フォルダの指定
 image_folder = "../VideoCapture/frame"
 
-# 現在時刻から15秒前の時刻を取得
-time_threshold = datetime.now() - timedelta(seconds=15)
-
-# base64にエンコードされた画像のリスト
-base64Images = []
-
-# 過去15秒間の画像を読み込む
-for i in range(15):  # 1〜15の画像をチェック
-  image_path = os.path.join(image_folder, f"saved_frame_{i}.jpg")
-  print(image_path)
-  if os.path.exists(image_path):
-    image = cv2.imread(image_path)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # BGRからRGBに変換
-    is_success, im_buf_arr = cv2.imencode(".png", image)  # 画像をPNG形式に変換
-    byte_im = im_buf_arr.tobytes()
-    base64Images.append(base64.b64encode(byte_im).decode("utf-8"))
-
 # GPT-4ビジョンモデルへのプロンプト
 PROMPT_CONTENT = """
-画像の内容を読み取って、何が写っているのか説明してください。256トークンのため途中で切れないように注意してください。
+これはVAMPIRESURVIVORのゲーム実況です。
+ゲーム実況をする特にキャラ付けのないOLのようなVTuberが読み上げるためのナレーションスクリプトのみを作成してください。
+テンションは低めで4行程度でお願いします。
 """
 
-PROMPT_MESSAGES = [
-    {
-        "role": "user",
-        "content": [
-            PROMPT_CONTENT,
-            *map(lambda x: {"image": x}, base64Images),
-        ],
-    },
-]
+# APIパラメータの設定
+api_key = os.environ['API_KEY']
 
-params = {
-    "model": "gpt-4-vision-preview",
-    "messages": PROMPT_MESSAGES,
-    "api_key": os.environ['API_KEY'],
-    "headers": {"Openai-Version": "2020-11-07"},
-    "max_tokens": 256,
-}
+while True:
+    # 画像を読み込んでbase64にエンコード
+    image_path = os.path.join(image_folder, f"saved_frame_0.jpg")
+    base64_image = encode_image_to_base64(image_path)
 
-# OpenAI APIにリクエストを送信
-result = openai.ChatCompletion.create(**params)
-print(result.choices[0].message.content)
+    print(image_path)
+
+    PROMPT_MESSAGES = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": PROMPT_CONTENT},  # ここに質問を書く
+                {"type": "image_url", "image_url": f"data:image/jpeg;base64,{base64_image}"},  # 画像の指定の仕方がちょい複雑
+            ],
+        },
+    ]
+
+    params = {
+        "model": "gpt-4-vision-preview",
+        "messages": PROMPT_MESSAGES,
+        "api_key": api_key,
+        "max_tokens": 300,
+    }
+
+    # OpenAI APIにリクエストを送信
+    start_time = time.time()
+    result = openai.ChatCompletion.create(**params)
+    end_time = time.time()
+    print(result.choices[0].message.content)
+    print(f"実行時間（s）: {end_time - start_time} sec")
+
+    # 15秒待機
+    time.sleep(2)
